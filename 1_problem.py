357ce7bb8884e45471e580b2fa056bb83690df5b
OMPRAKASH DHANAK <ronakdhanak795@gmail.com>
2026-07-08 21:05:10 +0530

    Creating table of content with Dataframe

---
+++ 1_problem.py
@@ -0,0 +1,7 @@
+import pandas as pd
+info = {
+   "Name": ["Rahul","Harshita","Shradha"],
+   "CGPA": [9.3,7.5,8.6]
+}
+df = pd.DataFrame(info) # pd.Dataframe is an important datastructure for making table of content
+print(df)
